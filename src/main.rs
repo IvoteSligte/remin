96a7f1017859cbb703caee73d297f790288ec150
Ivo te Sligte <ivotesligte@gmail.com>
2026-07-02 13:43:32 +0200

    client/server screens

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,89 @@
+use enigo::{
+    Direction::{Press, Release},
+    Enigo, Key, Keyboard, Settings,
+};
+use std::sync::{Arc, Mutex};
+
 slint::slint! {
+    import { Button } from "std-widgets.slint";
     export component App inherits Window {
+        property <string> view: "menu";
+
+        if view == "menu" : Rectangle {
+            HorizontalLayout {
+                spacing: 10px;
+                Button {
+                    text: "Start Server";
+                    clicked => {
+                        debug("Pressed Start Server");
+                        root.view = "server";
+                    }
+                }
+                Button {
+                    text: "Start Client";
+                    clicked => {
+                        debug("Pressed Start Client");
+                        root.view = "client";
+                    }
+                }                
+            }
+        }
+
         callback key_pressed(string);
         callback key_released(string);
+        forward-focus: scope;
 
-        forward-focus: key-handler;
-            key-handler := FocusScope {
-
+        scope := FocusScope {
+            KeyBinding {
+                keys: @keys(Escape);
+                activated => {
+                    debug("Escape Pressed");
+                    root.view = "menu";
+                }
+            }
             key-pressed(event) => {
-                debug(event.text);
-                root.key_pressed(event.text);
+                if (view == "client" && !event.modifiers.meta && event.text != "") {
+                    root.key_pressed(event.text);
+                }
                 accept
             }
             key-released(event) => {
-                debug(event.text);
-                root.key_released(event.text);
+                if (view == "client" && !event.modifiers.meta && event.text != "") {
+                    root.key_released(event.text);
+                }                
                 accept
             }
+        }
+
+        if view == "client" : Rectangle {
+            Text { text: "Client"; }
+        }
+
+        if view == "server" : Rectangle {
+            Text { text: "Server"; }
         }
     }
 }
 
 fn main() {
     let app = App::new().unwrap();
-    app.on_key_pressed(|s| println!("key pressed: `{}`", s));
-    app.on_key_released(|s| println!("key pressed: `{}`", s));
+    let enigo = Arc::new(Mutex::new(Enigo::new(&Settings::default()).unwrap()));
+    let enigo2 = enigo.clone();
+    app.on_key_pressed(move |s| {
+        let Some(c) = s.chars().next() else {
+            return;
+        };
+        if let Err(err) = enigo.lock().unwrap().key(Key::Unicode(c), Press) {
+            eprintln!("Failed to press key '{}'. Error: {}", c, err);
+        }
+    });
+    app.on_key_released(move |s| {
+        let Some(c) = s.chars().next() else {
+            return;
+        };
+        if let Err(err) = enigo2.lock().unwrap().key(Key::Unicode(c), Release) {
+            eprintln!("Failed to release key '{}'. Error: {}", c, err);
+        }
+    });
     app.run().unwrap();
 }
