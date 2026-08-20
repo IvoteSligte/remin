8999f8e6e8b808dc8fabdab261d1ffe6e1867312
Ivo te Sligte <ivotesligte@gmail.com>
2026-08-20 17:43:34 +0200

    handle cpal stream invalidation

--- src/watcher/audio.rs
+++ src/watcher/audio.rs
@@ -1,16 +1,19 @@
-use std::time::Instant;
+use std::{sync::mpsc, time::Instant};
 
-use anyhow::anyhow;
+use anyhow::{Context, bail};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use log::{debug, info, warn};
+use log::{debug, error, warn};
 use netnet::UnreliableReceiver;
 
 use crate::common::{Opus, TimeStamp, since};
 
 struct AudioPlayback {
     writer: rtrb::Producer<i16>,
-    error_receiver: std::sync::mpsc::Receiver<cpal::Error>,
-    _stream: cpal::Stream,
+    error_receiver: mpsc::Receiver<cpal::Error>,
+    /// The stream can temporarily be [None] after being invalidated by `cpal`.
+    stream: Option<cpal::Stream>,
+    channels: u32,
+    sample_rate: u32,
 }
 
 impl AudioPlayback {
@@ -18,7 +21,7 @@ impl AudioPlayback {
         let host = cpal::default_host();
         let Some(device) = host.default_output_device() else {
             warn!("No audio output device found");
-            return Err(anyhow!("No audio output device found"));
+            bail!("No audio output device found");
         };
         for config in device.supported_output_configs()? {
             if config.channels() as u32 != channels
@@ -27,38 +30,53 @@ impl AudioPlayback {
             {
                 continue;
             }
-            let (error_sender, error_receiver) = std::sync::mpsc::sync_channel::<cpal::Error>(0);
+            let (error_sender, error_receiver) = mpsc::sync_channel::<cpal::Error>(0);
             let (writer, mut reader) = rtrb::RingBuffer::new(20_000);
             let stream = device.build_output_stream(
                 config.with_sample_rate(sample_rate).into(),
                 move |output, _info| {
-                    let (_, remainder) = reader.pop_partial_slice(output);
+                    let (filled, remainder) = reader.pop_partial_slice(output);
                     if !remainder.is_empty() {
-                        info!("Not enough audio data to fill the target buffer");
+                        debug!(
+                            "Not enough audio data to fill the target buffer ({}/{} samples written)",
+                            filled.len(),
+                            output.len()
+                        );
                     }
                 },
                 move |err| error_sender.send(err).unwrap(),
                 None,
             )?;
-            stream.play()?;
+            stream
+                .play()
+                .inspect_err(|err| error!("Failed to start playback stream: {err}"))?;
             return Ok(Self {
                 writer,
                 error_receiver,
-                _stream: stream,
+                stream: Some(stream),
+                channels,
+                sample_rate,
             });
         }
-        Err(anyhow!(
-            "No audio config found that matches the desired channel count and sample rate"
-        ))
+        bail!("No audio config found that matches the desired channel count and sample rate")
     }
 
-    pub fn get_error(&self) -> anyhow::Result<()> {
+    pub fn get_error(&mut self) -> anyhow::Result<()> {
+        if self.stream.is_none() {
+            error!("Tried to write chunk to invalidated stream.");
+            return Ok(());
+        }
         match self.error_receiver.try_recv() {
-            Ok(err) => Err(err.into()),
-            Err(std::sync::mpsc::TryRecvError::Empty) => Ok(()),
-            Err(std::sync::mpsc::TryRecvError::Disconnected) => {
-                Err(anyhow!("Cpal thread panicked"))
+            Ok(err) if err.kind() == cpal::ErrorKind::StreamInvalidated => {
+                error!("Playback stream invalidated: {err}");
+                drop(self.stream.take());
+                *self = Self::new(self.channels, self.sample_rate)
+                    .context("When trying to recreate an invalidated playback stream")?;
+                Ok(())
             }
+            Ok(err) => Err(err.into()),
+            Err(mpsc::TryRecvError::Empty) => Ok(()),
+            Err(mpsc::TryRecvError::Disconnected) => bail!("Cpal thread panicked"),
         }
     }
 
